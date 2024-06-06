89d866e039900959c467a2c3bec80bfee60e9abe
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-06-06 13:27:57 +0900

    [wip] edit get_a_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,36 +6,51 @@
 /*   By: mishimod <mishimod@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 22:25:02 by mishimod          #+#    #+#             */
-/*   Updated: 2024/06/05 23:32:42 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/06/06 13:27:44 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *road_file(int fd, char *str)
-{}
-
-char *get_a_line(*str)
+char	*road_file(int fd, char *str)
 {
-	size_t i;
-
-	while(str[i])
 }
 
-char *road_next_line(*str)
-{}
+char	*get_a_line(*str)
+{
+	size_t	i;
+	size_t	flag;
+	char	*buf;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	flag = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		flag = 1;
+	buf = (char *)malloc(sizeof char) * (i + flag + 1);
+	if (!buf)
+		return (NULL);
+	ft_strlcpy(buf, str, i + flag + 1);
+	return (buf);
+}
+
+char	*road_next_line(*str)
+{
+}
 
 char	*get_next_line(int fd)
 {
-	static char *str;
-	char *line;
+	static char	*str;
+	char		*line;
 
 	if (0 > fd || 0 >= BUFFER_SIZE)
 		return (NULL);
-	str = 
-	if (!str)
-		return (NULL);
-	line =
 	str =
-	return (line);
+		if (!str) return (NULL);
+	line =
+		str =
+			return (line);
 }
