7904749b60f6c360bb652bcaae6d4d9bb9b969de
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-06-06 13:07:13 +0900

    [WIP] edit get_a_line(), get_next_line()

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,19 +6,23 @@
 /*   By: mishimod <mishimod@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 22:25:02 by mishimod          #+#    #+#             */
-/*   Updated: 2024/06/05 18:56:42 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/06/05 23:32:42 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *()
+char *road_file(int fd, char *str)
 {}
 
-char *()
-{}
+char *get_a_line(*str)
+{
+	size_t i;
 
-char *()
+	while(str[i])
+}
+
+char *road_next_line(*str)
 {}
 
 char	*get_next_line(int fd)
