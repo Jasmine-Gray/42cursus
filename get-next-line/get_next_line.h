519515d9947fe7af1e630687db85a465cb5e1b94
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-07-04 22:46:55 +0900

    [impl] get-next-line

---
+++ get-next-line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mishimod <mishimod@student.42.jp>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 19:54:08 by mishimod          #+#    #+#             */
+/*   Updated: 2024/07/04 20:06:25 by mishimod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <limits.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *str);
+
+#endif
