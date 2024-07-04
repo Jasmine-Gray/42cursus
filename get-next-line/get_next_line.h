6bfca18724621d07f71841fc1dea96e2d699e81a
Yuki Shimoda <yuki-shimoda@protonmail.com>
2024-07-05 01:49:48 +0900

    [fix]: add const

--- get-next-line/get_next_line.h
+++ get-next-line/get_next_line.h
@@ -24,9 +24,9 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
-char	*ft_strchr(char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *str);
 
 #endif
