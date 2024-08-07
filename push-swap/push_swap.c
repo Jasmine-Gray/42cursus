1b438138f7ec25f6d602f84385fb95cbbc93ec66
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-08-07 16:30:26 +0900

    [fix] ft swap

--- push-swap/push_swap.c
+++ push-swap/push_swap.c
@@ -6,13 +6,13 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 18:21:02 by mishimod          #+#    #+#             */
-/*   Updated: 2024/08/07 13:15:57 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/08/07 16:30:16 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-push_swap(a)
+push_swap(stack a)
 {
 
 }
