5406e58fa7fae475340d64fa90af957bc4063372
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-10-07 15:22:16 +0900

    [wip] memo .h file

--- memo/Rank02/push_swap.md
+++ memo/Rank02/push_swap.md
@@ -7,10 +7,21 @@
 1のnextが2、prevは6にならなければならない
 6のnextが1、prevは5にならなければならない
 ```
+- 構造体
+		next
+		prev
 
 ### コマンドを作成
 
 `pa pb sa sb ss ra rb rr rra rrb rrr pa pb`
+
+### 双方好循環リストのライブラリ作成
+入ってきた分のnodeを作る
+- 引数で受け取った値をnodeにする... 
+t_node *ft_double_list_new(int value);
+- listの最下層に新しいnodeを連結させる関数... 
+t_stack *ft_double_list_add_back(t_node *new_node);
+if stackがnullの場合. stackがtopだけの場合. topにprevの値が入っている場合
 
 ### ソートの実装
 
--- push-swap/includes/push_swap.h
+++ push-swap/includes/push_swap.h
@@ -6,17 +6,25 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:16:21 by mishimod          #+#    #+#             */
-/*   Updated: 2024/08/19 13:36:19 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/09/22 19:05:57 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct s_list 
+typedef struct s_node
 {
+		int value;
+		struct s_node *prev;
+		struct s_node *next;
 
-} t_list;
+} t_node
 
+typedef struct s_linked_list
+{
+		t_node *top;
+
+} t_linked_list
 
 #endif
