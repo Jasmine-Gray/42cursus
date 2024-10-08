28225b26498902a89f410bd80eeeda81da275d43
Mio Shimoda <mishimod@c2r5s10.42tokyo.jp>
2024-10-08 20:02:27 +0900

    [wip] double_linked_list

--- push-swap/includes/push_swap.h
+++ push-swap/includes/push_swap.h
@@ -6,25 +6,31 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:16:21 by mishimod          #+#    #+#             */
-/*   Updated: 2024/10/07 22:37:07 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/10/08 18:40:18 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct s_stack
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_node
 {
 		int value;
-		struct s_stack *prev;
-		struct s_stack *next;
+		struct s_node *prev;
+		struct s_node *next;
 
-} t_stack
+} t_node;
 
 typedef struct s_linked_list
 {
-		t_stack *top;
+		t_node *top;
 
-} t_linked_list
+} t_linked_list;
+
+t_node *node_new(int value);
+t_linked_list *double_list_add_back(t_linked_list *stack, t_node *new_node);
 
 #endif
--- push-swap/srcs/lists/double_list_add_back.c
+++ push-swap/srcs/lists/double_list_add_back.c
@@ -6,38 +6,81 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:54:42 by mishimod          #+#    #+#             */
-/*   Updated: 2024/10/07 22:55:18 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/10/08 20:00:10 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack *double_list_add_back(t_stack *new_node)
+t_node	*find_last_node(t_linked_list *stack)
 {
-		t_stack *last;
-		t_stack **list;
-		
+	t_node	*current;
 
-		if (!list || !new_node)
-		{
-				return ;
-		}
+	if (!stack)
+		return (NULL);
+	if (!stack->top)
+		return (NULL);
+	if (stack->top->next == NULL)
+		return (stack->top);
+	current = stack->top;
+	while (!(current->next == stack->top))
+	{
+		current = current->next;
+	}
+	return (current);
+}
 
-		if (!*list)
-		{
-				*list = new_node;
-		}
+t_linked_list	*double_list_add_back(t_linked_list *stack, t_node *new_node)
+{
+	t_node	*last;
 
+	if (!stack || !new_node)
+	{
+		return (NULL);
+	}
+	if (!(stack->top)) // stack no naka no top ga nai toki
+	{
+		stack->top = new_node; // stack no top no naka ni new_node wo dainyuu
+		return (stack);
+	}
+	if (stack->top->next == NULL)
+	{
+		stack->top->next = new_node;
+		stack->top->prev = new_node;
+		new_node->next = stack->top;
+		new_node->prev = stack->top;
+	}
+	last = find_last_node(stack);
+	last->next = new_node;
+	stack->top->prev = new_node;
+	new_node->next = stack->top;
+	new_node->prev = last;
+	return (stack);
+}
 
-		if (stackがtopだけの場合)
-		{
-		}
+int	main(void)
+{
+	t_linked_list	stack;
+	t_linked_list	*result;
+	t_node			*node_1;
+	t_node			*node_2;
+	t_node			*node_3;
+	t_node			*node_4;
 
-		else if (topにprevの値が入っている場合)
-		{
+	stack.top = NULL;
+	node_1 = node_new(12);
+	node_2 = node_new(24);
+	node_3 = node_new(42);
+	node_4 = node_new(26);
+    double_list_add_back(&stack, node_1);
+    double_list_add_back(&stack, node_2);
+    double_list_add_back(&stack, node_3);
+    double_list_add_back(&stack, node_4);
 
-		}
-		last->next = new_node;
-		new_node->prev = last;
-		return ;
+	printf("stack=%d->%d->%d->%d\n", stack.top->value, stack.top->next->value, stack.top->next->next->value, stack.top->next->next->next->value); // . ha kouzoutai no senngen ga hennsuu ->ha pointer 
+	free(node_1);
+	free(node_2);
+	free(node_3);
+	free(node_4);
+    return (0);
 }
