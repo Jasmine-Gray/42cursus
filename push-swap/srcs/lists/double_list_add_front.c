953844cacb7305e35bf3f13ad1e9f0604dca1af4
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-10-11 16:11:40 +0900

    [init commit] d_list_add_front

---
+++ push-swap/srcs/lists/double_list_add_front.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   double_list_add_front.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 00:36:43 by mishimod          #+#    #+#             */
+/*   Updated: 2024/10/11 00:45:28 by mishimod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_linked_list	*double_list_add_front(t_linked_list *stack, t_node *new_node)
+{
+	if (!stack || !new_node)
+		return (NULL);
+
+}
