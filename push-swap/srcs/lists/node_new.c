fcbecb68777c49ab73601827bf9c2b5090972833
Mio Shimoda <mishimod@c2r6s7.42tokyo.jp>
2024-10-17 21:30:40 +0900

    [wip]

--- push-swap/srcs/lists/node_new.c
+++ push-swap/srcs/lists/node_new.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 13:04:29 by mishimod          #+#    #+#             */
-/*   Updated: 2024/10/14 20:50:38 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/10/17 15:26:31 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ t_node	*node_new(int value)
 
 	new = malloc(sizeof(t_node));
 	if (!new)
+	{
 		return (NULL);
+		// free (new);
+	}
 	new->value = value;
 	new->prev = NULL;
 	new->next = NULL;
 	return (new);
+	// free(new);
 }
--- push-swap/srcs/utils/build_stack.c
+++ push-swap/srcs/utils/build_stack.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 17:15:18 by mishimod          #+#    #+#             */
-/*   Updated: 2024/10/16 01:14:43 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/10/17 15:19:37 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	build_stack(int argc, const char **argv_str, t_linked_list *stack)
 {
 	t_node	*tmp;
 	size_t			i;
-
 
 	i = 0;
 	while (argv_str[i])
