3a9132c412192676765584fc25e0f729f285b3bb
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-10-26 08:11:21 +0900

    [wip]

--- push-swap/srcs/sort/sort_utils.c
+++ push-swap/srcs/sort/sort_utils.c
@@ -6,16 +6,15 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 20:53:37 by mishimod          #+#    #+#             */
-/*   Updated: 2024/10/24 21:29:29 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/10/26 00:12:26 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
 void	retain_value(t_node *left, t_node *right)
 {
-	t_node *tmp;
+	t_node	*tmp;
 
 	tmp = left;
 	left = right;
@@ -31,21 +30,23 @@ void	retain_pivot(t_node *left, t_node *pivot)
 	pivot = tmp;
 }
 
-// int	measure_distance(t_linked_list *stack, t_node *left)
-// {
-// 	t_linked_list	*stack_a;
-// 	int				distance;
-// 
-//     count_node(stack_a);
-// 
-// 	stack_a = stack;
-// 	distance = 0;
-// 	if (!stack_a)
-// 		return ;
-// 	while (!last)
-// 	{
-// 		distance++;
-// 		stack_a->top->next;
-// 	}
-// 	return (distance);
-// }
+int	measure_distance(t_linked_list *stack, t_node *left)
+{
+	t_linked_list	*stack_a;
+	int				count;
+	int				distance;
+	int				result;
+
+	stack_a = stack;
+	if (!stack_a)
+		return ;
+	count = count_node(stack_a);
+	distance = 0;
+	while (!left)
+	{
+		distance++;
+		stack_a->top->next;
+	}
+	result = count / left;
+	return (result);
+}
