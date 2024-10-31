7a75cd64c3cd1896297aa610e2e5f66b818987ff
Mio Shimoda <mishimod@c2r6s5.42tokyo.jp>
2024-10-31 16:51:19 +0900

    [wip]

--- push-swap/srcs/sort/swap_left_right.c
+++ push-swap/srcs/sort/swap_left_right.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:56:10 by mishimod          #+#    #+#             */
-/*   Updated: 2024/10/31 16:08:44 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/10/31 16:50:47 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ static void	close_to_top(t_linked_list *stack_a, t_linked_list *stack_b,
 		t_node *left, t_node *right)
 {
 	int	count;
+	if (stack_b != NULL)
+			return ;
 
 	while (!(left == stack_a->top))
 	{
@@ -38,10 +40,14 @@ static void	close_to_top(t_linked_list *stack_a, t_linked_list *stack_b,
 		rotate_a(stack_a);
 		count--;
 	}
+	printf("1sortleft=%d\n", left->value);
+	printf("1sortright=%d\n", right->value);
+	if (stack_b == NULL)
+			printf("OK\n");
 	while (stack_b != NULL)
 		push_a(stack_a, stack_b);
-	printf("sortright=%d\n", right->value);
-	printf("sortleft=%d\n", left->value);
+	printf("2sortleft=%d\n", left->value);
+	printf("2sortright=%d\n", right->value);
 }
 
 static void	close_to_bottom(t_linked_list *stack_a, t_linked_list *stack_b,
