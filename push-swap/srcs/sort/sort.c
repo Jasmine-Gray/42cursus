40c80e058c125b15b137278141215c7da8245a2b
Mio Shimoda <mishimod@c2r6s7.42tokyo.jp>
2024-11-07 14:48:56 +0900

    [wip]

--- push-swap/srcs/sort/sort.c
+++ push-swap/srcs/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:29:04 by mishimod          #+#    #+#             */
-/*   Updated: 2024/11/05 20:47:38 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/11/06 18:04:43 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	main_sort(t_linked_list *stack_a, t_linked_list *stack_b, t_node *left,
 	t_node	partition;
 	t_node	*right;
 
-	partition = *pivot;
+    partition = *left;
 	right = pivot->prev;
 	if (!stack_a || !left || !right || !pivot)
 		return ;
@@ -29,7 +29,7 @@ void	main_sort(t_linked_list *stack_a, t_linked_list *stack_b, t_node *left,
 		if (left->next == right->next)
 		{
 			swap_left_pivot(stack_a, stack_b, &left, &pivot);
-			partition = *left;
+		    partition = *left;
 			// break ;
 		}
 		if (left->value > pivot->value)
@@ -39,19 +39,21 @@ void	main_sort(t_linked_list *stack_a, t_linked_list *stack_b, t_node *left,
 			if (right->value < pivot->value)
 			{
 				swap_left_right(stack_a, stack_b, &left, &right);
+				partition = *left;
 				// break ;
 			}
 			if (left->prev == right)
 			{
 				insert_pivot(stack_a, stack_b, &left);
+				partition = *left;
 				// break ;
 			}
 		}
 	}
-	if (partition.value != pivot->value)
+	if (partition.value == pivot->value)
 			main_sort(stack_a, stack_b, left, pivot);
-	if (partition.value != left->value)
-			main_sort(stack_a, stack_b, partition.prev, left);
+	if (partition.value == left->value)
+			main_sort(stack_a, stack_b, stack_a->top, left);
 	print_stack(stack_a);
 }
 
