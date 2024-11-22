9a019d1370529d34de8a4b975f7a7f97be45fcb3
Mio Shimoda <mishimod@c2r6s3.42tokyo.jp>
2024-11-22 13:51:50 +0900

    [init commit] index_stack

---
+++ push-swap/srcs/sort/index_stack.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index_stack.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 13:31:42 by mishimod          #+#    #+#             */
+/*   Updated: 2024/11/22 13:49:15 by mishimod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void index_stack(t_linked_list *stack_a)
+{
+		t_node *current;
+		t_node *last;
+		int index;
+
+		current = stack_a->top;
+		last = find_last_node(stack_a);
+		index = 0;
+
+		while(current->next != )
+		{
+				if (current > current->value)
+				{
+						
+				}
+		}
+}
