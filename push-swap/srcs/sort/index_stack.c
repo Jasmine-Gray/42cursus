56404f4ce9e7fbba42d318de4aa607b4864a5752
Mio Shimoda <mishimod@c2r6s3.42tokyo.jp>
2024-11-22 16:49:25 +0900

    [wip]

--- push-swap/srcs/sort/index_stack.c
+++ push-swap/srcs/sort/index_stack.c
@@ -6,27 +6,31 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:31:42 by mishimod          #+#    #+#             */
-/*   Updated: 2024/11/22 13:49:15 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/11/22 16:49:14 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void index_stack(t_linked_list *stack_a)
+void	index_stack(t_linked_list *stack_a)
 {
-		t_node *current;
-		t_node *last;
-		int index;
+	t_node	*current;
+	t_node	*last;
+	t_node	*min;
+	int		index;
 
-		current = stack_a->top;
-		last = find_last_node(stack_a);
-		index = 0;
+	current = stack_a->top;
+	last = find_last_node(stack_a);
+	min = current;
+	index = 0;
 
-		while(current->next != )
+	while (current != last)
+	{
+		if (min->value > current->next->value)
 		{
-				if (current > current->value)
-				{
-						
-				}
+			min = current->next;
+			index = min->value;
 		}
+		current = current->next;
+	}
 }
