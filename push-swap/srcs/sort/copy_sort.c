2b56683c41359f4ec891226a2cbf2a46f30cb839
Mio Shimoda <mishimod@c2r5s4.42tokyo.jp>
2024-11-26 16:53:24 +0900

    [fix] copy_sort

--- push-swap/srcs/sort/copy_sort.c
+++ push-swap/srcs/sort/copy_sort.c
@@ -6,23 +6,25 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:29:04 by mishimod          #+#    #+#             */
-/*   Updated: 2024/11/26 15:42:40 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/11/26 16:52:08 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	main_sort(t_linked_list *stack_a, t_linked_list *stack_b, t_node *left,
-		t_node *pivot)
+void	main_sort(t_linked_list *stack_a, t_linked_list *stack_b) 
 {
+	t_node	*left;
 	t_node	*right;
-	// t_node* left, pivot = NULL; //arg pivot left
-	right = pivot->prev;
-	if (!stack_a || !left || !right || !pivot)
+	t_node	*pivot;
+	left = NULL;
+	pivot = NULL;
+	if (!stack_a)
 		return ;
 	while (is_sort(stack_a) == 0)
 	{
 		position_pivot(stack_a, &pivot);
+		right = pivot->prev;
 		//lldb de check subeki right ha right = stack->top
 		while (left->prev != right)
 		{
@@ -56,5 +58,5 @@ void	sort(t_linked_list *stack_a, t_linked_list *stack_b)
 		return ;
 	pivot = find_last_node(stack_a);
 	left = stack_a->top;
-	main_sort(stack_a, stack_b, left, pivot);
+	main_sort(stack_a, stack_b);
 }
