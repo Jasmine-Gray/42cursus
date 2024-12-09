6ebcf5407dff70a51d023bb2e42f697beb3a620e
Mio Shimoda <mishimod@c2r6s3.42tokyo.jp>
2024-12-09 20:40:04 +0900

    [wip] tidy dir

--- push-swap/srcs/sort/insert_pivot.c
+++ push-swap/srcs/sort/insert_pivot.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 20:58:19 by mishimod          #+#    #+#             */
-/*   Updated: 2024/12/09 18:03:22 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/12/09 19:39:04 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,45 +57,45 @@ static void	close_to_top(t_linked_list *stack_a, t_linked_list *stack_b,
 	}
 }
 
-static void	close_to_bottom(t_linked_list *stack_a, t_linked_list *stack_b,
-		t_node **left, t_node **pivot)
-{
-	int		count;
-	int		num;
-	t_node	*last;
-
-	last = find_last_node(stack_a);
-	count = 0;
-	while ((*pivot) != last)
-	{
-		reverse_rotate_a(stack_a);
-		count++;
-	}
-	while (count != 0)
-	{
-		push_b(stack_a, stack_b);
-		count--;
-	}
-	reverse_rotate_a(stack_a);
-	push_b(stack_a, stack_b);
-	num = 1;
-	while (*left != stack_a->top)
-	{
-		reverse_rotate_a(stack_a);
-		num++;
-	}
-	push_a(stack_a, stack_b);
-	while (num)
-	{
-		rotate_a(stack_a);
-		num--;
-	}
-	while (stack_b != NULL)
-	{
-		push_a(stack_a, stack_b);
-		rotate_a(stack_a);
-	}
-}
+// static void	close_to_bottom(t_linked_list *stack_a, t_linked_list *stack_b,
+// 		t_node **left, t_node **pivot)
+// {
+// 	int		count;
+// 	int		num;
+// 	t_node	*last;
+// 
+// 	last = find_last_node(stack_a);
+// 	count = 0;
+// 	while ((*pivot) != last)
+// 	{
+// 		reverse_rotate_a(stack_a);
+// 		count++;
+// 	}
+// 	while (count != 0)
+// 	{
+// 		push_b(stack_a, stack_b);
+// 		count--;
+// 	}
+// 	reverse_rotate_a(stack_a);
+// 	push_b(stack_a, stack_b);
+// 	num = 1;
+// 	while (*left != stack_a->top)
+// 	{
+// 		reverse_rotate_a(stack_a);
+// 		num++;
+// 	}
+// 	push_a(stack_a, stack_b);
+// 	while (num)
+// 	{
+// 		rotate_a(stack_a);
+// 		num--;
+// 	}
+// 	while (stack_b != NULL)
+// 	{
+// 		push_a(stack_a, stack_b);
+// 		rotate_a(stack_a);
+// 	}
+// }
 
 void	insert_pivot(t_linked_list *stack_a, t_linked_list *stack_b,
 		t_node **left, t_node **pivot)
@@ -111,6 +111,7 @@ void	insert_pivot(t_linked_list *stack_a, t_linked_list *stack_b,
 	}
 	if (distance > 75)
 	{
-		close_to_bottom(stack_a, stack_b, left, pivot);
+		close_to_top(stack_a, stack_b, left, pivot);
+		//close_to_bottom(stack_a, stack_b, left, pivot);
 	}
 }
--- push-swap/srcs/sort/swap_left_right.c
+++ push-swap/srcs/sort/swap_left_right.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:56:10 by mishimod          #+#    #+#             */
-/*   Updated: 2024/12/08 19:18:46 by mishimod         ###   ########.fr       */
+/*   Updated: 2024/12/09 19:37:23 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,34 +48,34 @@ static void	close_to_top(t_linked_list *stack_a, t_linked_list *stack_b,
 	}
 }
 
-static void	close_to_bottom(t_linked_list *stack_a, t_linked_list *stack_b,
-		t_node **left, t_node **right)
-{
-	int	count;
-
-	while (*right == stack_b->top)
-	{
-		reverse_rotate_a(stack_a);
-		push_b(stack_a, stack_b);
-	}
-	count = 0;
-	while (!(*left = stack_a->top))
-	{
-		reverse_rotate_a(stack_a);
-		count++;
-	}
-	push_b(stack_a, stack_b);
-	swap_b(stack_b);
-	retain_value(left, right);
-	push_a(stack_a, stack_b);
-	while (count != 0)
-	{
-		rotate_a(stack_a);
-		count--;
-	}
-	while (stack_b)
-		push_a(stack_a, stack_b);
-}
+// static void	close_to_bottom(t_linked_list *stack_a, t_linked_list *stack_b,
+// 		t_node **left, t_node **right)
+// {
+// 	int	count;
+// 
+// 	while (*right == stack_b->top)
+// 	{
+// 		reverse_rotate_a(stack_a);
+// 		push_b(stack_a, stack_b);
+// 	}
+// 	count = 0;
+// 	while (!(*left = stack_a->top))
+// 	{
+// 		reverse_rotate_a(stack_a);
+// 		count++;
+// 	}
+// 	push_b(stack_a, stack_b);
+// 	swap_b(stack_b);
+// 	retain_value(left, right);
+// 	push_a(stack_a, stack_b);
+// 	while (count != 0)
+// 	{
+// 		rotate_a(stack_a);
+// 		count--;
+// 	}
+// 	while (stack_b)
+// 		push_a(stack_a, stack_b);
+// }
 
 void	swap_left_right(t_linked_list *stack_a, t_linked_list *stack_b,
 		t_node **left, t_node **right)
@@ -89,6 +89,7 @@ void	swap_left_right(t_linked_list *stack_a, t_linked_list *stack_b,
 	}
 	if (distance > 75)
 	{
-		close_to_bottom(stack_a, stack_b, left, right);
+		close_to_top(stack_a, stack_b, left, right);
+		//close_to_bottom(stack_a, stack_b, left, right);
 	}
 }
