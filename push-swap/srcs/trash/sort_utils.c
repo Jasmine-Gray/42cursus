0c4b2134e3e885bf6d665c9266b34fac4615fd06
Mio Shimoda <mishimod@c5r3s14.42tokyo.jp>
2025-01-08 23:07:03 +0900

    [mv] sort_utils

---
+++ push-swap/srcs/trash/sort_utils.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 20:53:37 by mishimod          #+#    #+#             */
+/*   Updated: 2024/12/21 02:39:04 by mishimod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	retain_value(t_node **left, t_node **right)
+{
+	t_node	*tmp;
+
+	tmp = *left;
+	*left = *right;
+	*right = tmp;
+}
+
+void	retain_pivot(t_node **left, t_node **pivot)
+{
+	t_node	*tmp;
+
+	tmp = *left;
+	*left = *pivot;
+	*pivot = tmp;
+}
+
+int	measure_distance(t_linked_list *stack, t_node **left)
+{
+	t_linked_list	*stack_a;
+	int				count;
+	int				distance;
+	int				result;
+	t_node			*current;
+
+	stack_a = stack;
+	if (!stack_a)
+		return (0);
+	count = count_node(stack_a);
+	distance = 0;
+	current = stack_a->top;
+	while (!left)
+	{
+		distance++;
+		current = current->next;
+	}
+	result = (distance / count) * 100;
+	return (result);
+}
+
+void set_flag(t_linked_list *stack_b, t_node **flag)
+{
+	if (!stack_b)
+	{
+			*flag = NULL;
+			return ;
+    }
+	if (!stack_b->top)
+	{
+			*flag = NULL;
+			return ;
+	}
+	*flag = stack_b->top;
+}
