603e1621c97d2dccdfb282d037476f0b5d38de1e
Mio Shimoda <mishimod@c1r7s2.42tokyo.jp>
2025-01-16 16:06:09 +0900

    [wip]

--- push-swap/srcs/sort/index_stack.c
+++ push-swap/srcs/sort/index_stack.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:31:42 by mishimod          #+#    #+#             */
-/*   Updated: 2025/01/08 22:25:53 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/01/16 16:04:16 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,31 @@ static int	find_index_zero(t_linked_list *stack_a)
 	return (0);
 }
 
-void	index_stack(t_linked_list *stack_a)
+static void	find_min_node(t_linked_list *stack_a, t_node **min_node,
+		t_node *last)
 {
 	t_node	*current;
+
+	current = stack_a->top;
+	while (1)
+	{
+		if (current->index == 0)
+			break ;
+		current = current->next;
+	}
+	*min_node = current;
+	while (1)
+	{
+		if (((*min_node)->value > current->value) && (current->index == 0))
+			*min_node = current;
+		if (current == last)
+			break ;
+		current = current->next;
+	}
+}
+
+void	index_stack(t_linked_list *stack_a)
+{
 	t_node	*last;
 	t_node	*min_node;
 	int		index;
@@ -45,22 +67,7 @@ void	index_stack(t_linked_list *stack_a)
 	index = 1;
 	while (find_index_zero(stack_a) == 1)
 	{
-		current = stack_a->top;
-		while (1)
-		{
-			if (current->index == 0)
-				break ;
-			current = current->next;
-		}
-		min_node = current;
-		while (1)
-		{
-			if ((min_node->value > current->value) && (current->index == 0))
-				min_node = current;
-			if (current == last)
-				break ;
-			current = current->next;
-		}
+		find_min_node(stack_a, &min_node, last);
 		min_node->index = index;
 		index++;
 	}
