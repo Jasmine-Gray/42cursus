08a30242008bb3a081ae01beb600704bba7dfd80
Mio Shimoda <mishimod@c1r7s2.42tokyo.jp>
2025-01-16 12:16:18 +0900

    [restore] ft_atoi

--- push-swap/libft/ft_atoi.c
+++ push-swap/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <mishimod@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 16:20:51 by mishimod          #+#    #+#             */
-/*   Updated: 2025/01/16 12:12:00 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/01/16 12:15:48 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 static char	ft_isspace(unsigned char s)
 {
-	return ((s >= 9 && s <= 13) || s == 32);
-	// 	return (1);
-	// return (NULL);
+	if ((s >= 9 && s <= 13) || s == 32)
+		return (1);
+	return (0);
 }
 
 static void	check_sign(const char *str, size_t *i, int *sign)
@@ -31,7 +31,7 @@ static void	check_sign(const char *str, size_t *i, int *sign)
 	}
 }
 
-int	*ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	unsigned long	result;
 	int				sign;
@@ -40,23 +40,19 @@ int	*ft_atoi(const char *str)
 	result = 0;
 	sign = 1;
 	i = 0;
-	if (!str)
-		return (NULL);
 	check_sign(str, &i, &sign);
 	while (str[i] >= '0' && str[i] <= '9' && str[i] != '\0')
 	{
 		if (sign == -1 && result > ((unsigned long)LONG_MAX + 1 - (str[i]
 					- '0')) / 10)
-			return (NULL);
+			return ((int)LONG_MIN);
 		if (sign != -1 && result > ((unsigned long)LONG_MAX - (str[i] - '0'))
 			/ 10)
-			return (NULL);
+			return ((int)LONG_MAX);
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	if (str[i] != '\0')
-		return (NULL);
-	return ((int *)(result * sign));
+	return (result * sign);
 }
 
 // bool check_atoi(void)
