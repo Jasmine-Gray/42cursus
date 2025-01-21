31112f22a103a16c70cb9f636daa17bcedd1fc6c
Mio Shimoda <mishimod@c1r5s3.42tokyo.jp>
2025-01-21 17:39:35 +0900

    [wip]

--- minitalk/srcs/client.c
+++ minitalk/srcs/client.c
@@ -6,13 +6,29 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:14 by mishimod          #+#    #+#             */
-/*   Updated: 2025/01/21 16:00:16 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/01/21 17:24:54 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(int argc, const char **argv)
+void ft_signal(const pid_t pid, char *str)
 {
 
+}
+
+int main(int argc, const char **argv)
+{
+		pid_t pid;
+		if (argc == 3)
+		{
+				pid = ft_atoi(argv[1]);
+				if (エラー処理)
+				{
 				}
+				ft_signal(pid, argv[2]);
+		}
+		else 
+				return (1);
+		return (0);
+}
--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,13 +6,13 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:32 by mishimod          #+#    #+#             */
-/*   Updated: 2025/01/21 16:00:16 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/01/21 16:44:27 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(int argc, const char **argv)
+int main(void)
 {
-
+		ft_printf("PID:%d\n", getpid());
 }
