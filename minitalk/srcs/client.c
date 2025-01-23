c5017351338fbdeb475fe22c862808c49b447de2
Mio Shimoda <mishimod@c1r3s1.42tokyo.jp>
2025-01-23 15:26:11 +0900

    [wip]

--- minitalk/srcs/client.c
+++ minitalk/srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:14 by mishimod          #+#    #+#             */
-/*   Updated: 2025/01/21 23:10:43 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/01/23 15:25:49 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,23 @@
 
 void ft_signal(const pid_t pid, char *str)
 {
-		int 
+		int
 		kill(pid, SIGUSR1);
 }
 
-int main(int argc, const char **argv)
+int	main(int argc, const char **argv)
 {
-		pid_t pid;
-		if (argc == 3)
-		{
-				pid = ft_atoi(argv[1]);//overflow
-				if (エラー処理)
-				{
-				}
-				ft_signal(pid, argv[2]);
-		}
-		else 
-				return (1);
-		return (0);
+	pid_t	pid;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]); // overflow
+		// if (エラー処理)
+		//{
+		//}
+		// ft_signal(pid, argv[2]);
+	}
+	else
+		return (1);
+	return (0);
 }
