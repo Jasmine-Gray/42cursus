e8c55d4d3d53fc9fdca7328426aed96650e0fdc8
Mio Shimoda <mishimod@c4r2s10.42tokyo.jp>
2025-02-03 18:32:10 +0900

    [add] memo [fix] bonus part

--- minitalk/srcs/client.c
+++ minitalk/srcs/client.c
@@ -6,11 +6,31 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:14 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/02 16:48:07 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/03 18:25:21 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static int send_null(pid_t pid)
+{
+		int bit;
+		int char_num;
+
+		bit = 0;
+		while(bit < 8)
+		{
+				char_num = kill(pid, SIGUSR2);
+				if (char_num == -1)
+				{
+						ft_printf("Error\n");
+						return (1);
+				}
+				usleep(500);
+				bit++;
+		}
+		return (0);
+}
 
 static int	send_char(const char **argv, pid_t pid)
 {
@@ -38,12 +58,19 @@ static int	send_char(const char **argv, pid_t pid)
 		}
 		i++;
 	}
-	return (0);
+	return (send_null(pid));
+}
+
+void handle_ack(int signum)
+{
+		if (signum == SIGUSR1)
+				ft_printf("acknowledgement received from server\n");
 }
 
 int	main(int argc, const char **argv)
 {
 	pid_t	pid;
+	struct sigaction sa;
 
 	if (argc != 3)
 	{
@@ -58,6 +85,10 @@ int	main(int argc, const char **argv)
 		ft_printf("Error\n");
 		return (1);
 	}
+	 sa.sa_handler = handle_ack;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
     send_char(argv, pid);
 	return (0);
 }
--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,16 +6,17 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:32 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/02 16:19:04 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/03 16:56:57 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void handle_signal(int signum)
+static void handle_signal(int signum, siginfo_t *info, void *context)
 {
 		static int i;
 		static int c;
+		(void)context;
 
 		i++;
 		if (signum == SIGUSR1)
@@ -24,7 +25,10 @@ static void handle_signal(int signum)
 				c = (c << 1) + 0;
 		if (i == 8)
 		{
-				write(1, &c, 1);
+				if (c == 0)
+						kill(info->si_pid, SIGUSR1);
+				else
+						write(1, &c, 1);
 				i = 0;
 				c = 0;
 		}
@@ -34,9 +38,9 @@ int	main(void)
 {
 	struct sigaction	sa;
 
-	sa.sa_handler = handle_signal;
+	sa.sa_sigaction = handle_signal;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
+	sa.sa_flags = SA_SIGINFO;
 	ft_printf("PID:%d\n", getpid());
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
