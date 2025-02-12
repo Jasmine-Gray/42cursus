ec17cdab2cb2097e9073fdcda0e18db1c16898f0
Mio Shimoda <mishimod@c5r3s10.42tokyo.jp>
2025-02-12 17:57:58 +0900

    [wip] can not send str

--- minitalk/srcs/client.c
+++ minitalk/srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:14 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/10 20:45:03 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:39:05 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,70 +16,64 @@ static volatile sig_atomic_t	g_ack_received;
 
 static void	send_char(const char **argv, pid_t pid)
 {
-	int	i;
-	int	bit;
+		int	i;
+		int	bit;
 
-	i = 0;
-	while (argv[2][i])
-	{
-		bit = 0;
-		while (bit < 8)
+		i = 0;
+		while (argv[2][i])
 		{
-			if ((argv[2][i] >> (7 - bit)) & 1)
-				kill(pid, SIGUSR1);//bit==1
-			else
-				kill(pid, SIGUSR2);//bit==0
-			while (!g_ack_received)
-				;
-			g_ack_received = 0;
-			usleep(500);
-			bit++;
+				bit = 7;
+				while (bit >= 0)
+				{
+						if (argv[2][i] & (1 << bit))
+								kill(pid, SIGUSR1);//bit==1
+						else
+								kill(pid, SIGUSR2);//bit==0
+						while (!g_ack_received)
+								;
+						g_ack_received = 0;
+						usleep(500);
+						bit--;
+				}
+				i++;
 		}
-		i++;
-	}
+		bit = 7;
+		while(bit >=0)
+		{
+				kill(pid, SIGUSR2);
+				while(!g_ack_received)
+						;
+				g_ack_received = 0;
+				usleep(500);
+				bit--;
+		}
 }
-
-// static void	send_null(pid_t pid)
-// {
-// 	int	bit;
-// 
-// 	bit = 8;
-// 	while (bit--)
-// 	{
-// 		kill(pid, SIGUSR2);
-// 		usleep(100);
-// 	}
-// }
 
 void	handle_ack(int signum)
 {
-	g_ack_received = signum;
+		g_ack_received = signum;
 }
 
 int	main(int argc, const char **argv)
 {
-	pid_t				pid;
-	struct sigaction	sa;
+		pid_t				pid;
 
-	if (argc != 3)
-	{
-		ft_printf("Error\n");
-		return (1);
-	}
-	pid = ft_atoi(argv[1]);
-	if (!pid)
-		return (1);
-	if (pid < 3)
-	{
-		ft_printf("Error\n");
-		return (1);
-	}
-	sa.sa_handler = handle_ack;
-	sa.sa_flags = 0;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
-	g_ack_received = 0;
-	send_char(argv, pid);
-	//send_null(pid);
-	return (0);
+		if (argc != 3)
+		{
+				ft_printf("Error\n");
+				return (1);
+		}
+		pid = ft_atoi(argv[1]);
+		if (!pid)
+				return (1);
+		if (pid < 3)
+		{
+				ft_printf("Error\n");
+				return (1);
+		}
+		signal(SIGUSR1, handle_ack);
+		signal(SIGUSR2, handle_ack);
+		g_ack_received = 0;
+		send_char(argv, pid);
+		return (0);
 }
--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,24 +6,24 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:32 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/10 20:34:50 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:57:48 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	rxBuffer;
+static int	g_rxbuffer;
 
 static void	handle_signal(int signum, siginfo_t *info, void *context)
 {
 	int	result;
 
 	(void)context;
-	rxBuffer = (rxBuffer << 1) | (signum - SIGUSR1);
-	if (rxBuffer & (1 << 8))
+	g_rxbuffer = (g_rxbuffer << 1) | (signum - SIGUSR1);
+	if (g_rxbuffer & (1 << 8))
 	{
-		result = write(1, &rxBuffer, 1);
-		rxBuffer = 1;
+		result = write(1, &g_rxbuffer, 1);
+		g_rxbuffer = 0;
 		if (result == -1)
 		{
 			kill(info->si_pid, SIGUSR2);
@@ -33,17 +33,17 @@ static void	handle_signal(int signum, siginfo_t *info, void *context)
 	kill(info->si_pid, SIGUSR1);
 }
 
-int	main(void)
+int	main(void)//work_in_progress
 {
 	struct sigaction	sa;
 
-	rxBuffer = 0;
-	sa.sa_sigaction = handle_signal;
+	g_rxbuffer = 0;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
-	ft_printf("PID:%d\n", getpid());
+	sa.sa_sigaction = handle_signal;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("PID:%d\n", getpid());
 	while (1)
 		pause();
 	return (0);
