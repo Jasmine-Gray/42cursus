30bd5f748d290fcfc878389555902ff962885d55
Mio Shimoda <mishimod@c5r3s5.42tokyo.jp>
2025-02-13 16:58:35 +0900

    [] send over 10000chars

--- minitalk/srcs/client.c
+++ minitalk/srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:14 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/12 17:39:05 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:55:41 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,64 +16,64 @@ static volatile sig_atomic_t	g_ack_received;
 
 static void	send_char(const char **argv, pid_t pid)
 {
-		int	i;
-		int	bit;
+	int	i;
+	int	bit;
 
-		i = 0;
-		while (argv[2][i])
-		{
-				bit = 7;
-				while (bit >= 0)
-				{
-						if (argv[2][i] & (1 << bit))
-								kill(pid, SIGUSR1);//bit==1
-						else
-								kill(pid, SIGUSR2);//bit==0
-						while (!g_ack_received)
-								;
-						g_ack_received = 0;
-						usleep(500);
-						bit--;
-				}
-				i++;
-		}
+	i = 0;
+	while (argv[2][i])
+	{
 		bit = 7;
-		while(bit >=0)
+		while (bit >= 0)
 		{
-				kill(pid, SIGUSR2);
-				while(!g_ack_received)
-						;
-				g_ack_received = 0;
-				usleep(500);
-				bit--;
+			if (argv[2][i] & (1 << bit))
+				kill(pid, SIGUSR1); // bit==1
+			else
+				kill(pid, SIGUSR2); // bit==0
+			while (!g_ack_received)
+				;
+			g_ack_received = 0;
+			usleep(100);
+			bit--;
 		}
+		i++;
+	}
+	bit = 7;
+	while (bit >= 0)
+	{
+		kill(pid, SIGUSR2);
+		while (!g_ack_received)
+			;
+		g_ack_received = 0;
+		usleep(500);
+		bit--;
+	}
 }
 
 void	handle_ack(int signum)
 {
-		g_ack_received = signum;
+	g_ack_received = signum;
 }
 
 int	main(int argc, const char **argv)
 {
-		pid_t				pid;
+	pid_t	pid;
 
-		if (argc != 3)
-		{
-				ft_printf("Error\n");
-				return (1);
-		}
-		pid = ft_atoi(argv[1]);
-		if (!pid)
-				return (1);
-		if (pid < 3)
-		{
-				ft_printf("Error\n");
-				return (1);
-		}
-		signal(SIGUSR1, handle_ack);
-		signal(SIGUSR2, handle_ack);
-		g_ack_received = 0;
-		send_char(argv, pid);
-		return (0);
+	if (argc != 3)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (!pid)
+		return (1);
+	if (pid < 3)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	signal(SIGUSR1, handle_ack);
+	signal(SIGUSR2, handle_ack);
+	g_ack_received = 0;
+	send_char(argv, pid);
+	return (0);
 }
--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:32 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/12 17:57:48 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:52:24 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,52 @@
 
 static int	g_rxbuffer;
 
+// static void	handle_signal(int signum, siginfo_t *info, void *context)
+// {
+// 	int	result;
+//
+// 	(void)context;
+// 	g_rxbuffer = (g_rxbuffer << 1) | (signum - SIGUSR1);
+// 	if (g_rxbuffer & (1 << 8))
+// 	{
+// 		result = write(1, &g_rxbuffer, 1);
+// 		g_rxbuffer = 0;
+// 		if (result == -1)
+// 		{
+// 			kill(info->si_pid, SIGUSR2);
+// 			return ;
+// 		}
+// 	}
+// 	kill(info->si_pid, SIGUSR1);
+// }
+
 static void	handle_signal(int signum, siginfo_t *info, void *context)
 {
-	int	result;
+	static int	bits_count = 0;
+	char		c;
+	int			result;
 
 	(void)context;
-	g_rxbuffer = (g_rxbuffer << 1) | (signum - SIGUSR1);
-	if (g_rxbuffer & (1 << 8))
+	g_rxbuffer = (g_rxbuffer << 1) | (signum == SIGUSR1);
+	bits_count++;
+	if (bits_count == 8) // 8ビット揃ったら出力
 	{
-		result = write(1, &g_rxbuffer, 1);
-		g_rxbuffer = 0;
+		c = g_rxbuffer;
+		result = write(1, &c, 1);
 		if (result == -1)
 		{
 			kill(info->si_pid, SIGUSR2);
 			return ;
 		}
+		bits_count = 0;
+		g_rxbuffer = 0;
 	}
 	kill(info->si_pid, SIGUSR1);
 }
 
-int	main(void)//work_in_progress
+int	main(void) // work_in_progress
 {
-	struct sigaction	sa;
+	struct sigaction sa;
 
 	g_rxbuffer = 0;
 	sigemptyset(&sa.sa_mask);
