66af93dab6591530f61187b5c060919a3ac583ad
Mio Shimoda <mishimod@c5r3s6.42tokyo.jp>
2025-02-13 20:45:47 +0900

    [submit]

--- minitalk/srcs/client.c
+++ minitalk/srcs/client.c
@@ -6,13 +6,31 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:14 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/13 16:55:41 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/13 19:20:43 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static volatile sig_atomic_t	g_ack_received;
+volatile sig_atomic_t	g_ack_received;
+
+static void	send_null(pid_t pid)
+{
+	int	bit;
+
+	bit = 7;
+	while (bit >= 0)
+	{
+		kill(pid, SIGUSR2);
+		while (!g_ack_received)
+			;
+		if ((bit == 0) && (g_ack_received))
+			ft_printf("acknowledgement received from server\n");
+		g_ack_received = 0;
+		usleep(100);
+		bit--;
+	}
+}
 
 static void	send_char(const char **argv, pid_t pid)
 {
@@ -26,9 +44,9 @@ static void	send_char(const char **argv, pid_t pid)
 		while (bit >= 0)
 		{
 			if (argv[2][i] & (1 << bit))
-				kill(pid, SIGUSR1); // bit==1
+				kill(pid, SIGUSR1);
 			else
-				kill(pid, SIGUSR2); // bit==0
+				kill(pid, SIGUSR2);
 			while (!g_ack_received)
 				;
 			g_ack_received = 0;
@@ -37,16 +55,7 @@ static void	send_char(const char **argv, pid_t pid)
 		}
 		i++;
 	}
-	bit = 7;
-	while (bit >= 0)
-	{
-		kill(pid, SIGUSR2);
-		while (!g_ack_received)
-			;
-		g_ack_received = 0;
-		usleep(500);
-		bit--;
-	}
+	send_null(pid);
 }
 
 void	handle_ack(int signum)
@@ -67,6 +76,11 @@ int	main(int argc, const char **argv)
 	if (!pid)
 		return (1);
 	if (pid < 3)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	if (kill(pid, 0) == -1)
 	{
 		ft_printf("Error\n");
 		return (1);
--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,32 +6,13 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 09:21:32 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/13 16:52:24 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/02/13 17:55:45 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 static int	g_rxbuffer;
-
-// static void	handle_signal(int signum, siginfo_t *info, void *context)
-// {
-// 	int	result;
-//
-// 	(void)context;
-// 	g_rxbuffer = (g_rxbuffer << 1) | (signum - SIGUSR1);
-// 	if (g_rxbuffer & (1 << 8))
-// 	{
-// 		result = write(1, &g_rxbuffer, 1);
-// 		g_rxbuffer = 0;
-// 		if (result == -1)
-// 		{
-// 			kill(info->si_pid, SIGUSR2);
-// 			return ;
-// 		}
-// 	}
-// 	kill(info->si_pid, SIGUSR1);
-// }
 
 static void	handle_signal(int signum, siginfo_t *info, void *context)
 {
@@ -42,7 +23,7 @@ static void	handle_signal(int signum, siginfo_t *info, void *context)
 	(void)context;
 	g_rxbuffer = (g_rxbuffer << 1) | (signum == SIGUSR1);
 	bits_count++;
-	if (bits_count == 8) // 8ビット揃ったら出力
+	if (bits_count == 8)
 	{
 		c = g_rxbuffer;
 		result = write(1, &c, 1);
@@ -57,9 +38,9 @@ static void	handle_signal(int signum, siginfo_t *info, void *context)
 	kill(info->si_pid, SIGUSR1);
 }
 
-int	main(void) // work_in_progress
+int	main(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
 	g_rxbuffer = 0;
 	sigemptyset(&sa.sa_mask);
