b942f01457ea287d25f22c96e0d2acaf181f88f7
Mio Shimoda <mishimod@c5r2s5.42tokyo.jp>
2025-02-16 21:22:58 +0900

    [start] fract-ol

--- fract-ol/srcs/mlx.c
+++ fract-ol/srcs/mlx.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fract_ol.h                                         :+:      :+:    :+:   */
+/*   mlx.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/13 20:58:26 by mishimod          #+#    #+#             */
-/*   Updated: 2025/02/16 20:52:30 by mishimod         ###   ########.fr       */
+/*   Created: 2025/02/16 20:53:35 by mishimod          #+#    #+#             */
+/*   Updated: 2025/02/16 20:57:06 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef "FRACT_OL"
-# define "FRACT_OL"
+#include "fract-ol.h"
 
-#include <mlx.h>
+void	imlx_init(void)
+{
+}
 
-#endif 
+void	*mlx_new_window(void *mlx_ptr, int size_x, int size_y, char *title);
+{
+}
+
+int		mlx_clear_window(void *mlx_ptr, void *win_ptr);
+{
+}
+
+int		mlx_destroy_window(void *mlx_ptr, void *win_ptr);
+{
+}
