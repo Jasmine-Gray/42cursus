2e6e7fffb4760fcf2acb8030ad0a2ee9f11525e3
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2025-03-30 00:29:31 +0900

    [wip]

--- fract-ol/srcs/main.c
+++ fract-ol/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 18:53:51 by mishimod          #+#    #+#             */
-/*   Updated: 2025/03/23 18:32:47 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/03/27 00:25:22 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int	main(void)
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	(void)mlx_win;
 	mlx_loop(mlx);
 }
