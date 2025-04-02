89602bb047394cacdd17faf41adc881f59f544d6
Mio Shimoda <mishimod@c5r2s5.42tokyo.jp>
2025-04-02 22:15:27 +0900

    [color expert]

--- fract-ol/includes/fractol.h
+++ fract-ol/includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 18:57:10 by mishimod          #+#    #+#             */
-/*   Updated: 2025/03/26 17:14:05 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/04/02 22:11:06 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,17 @@
 # include "libft.h"
 
 double	ft_atof(const char *str);
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+#define WIDTH 1000
+#define HEIGHT 1000
 
 #endif
