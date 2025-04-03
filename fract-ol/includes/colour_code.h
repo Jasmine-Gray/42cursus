6c3cc0d9134a6c3ce0542a6c39fa28f7f6b3ed6c
Mio Shimoda <mishimod@c5r1s1.42tokyo.jp>
2025-04-03 16:28:07 +0900

    [fix] colour_code.h

--- fract-ol/includes/colour_code.h
+++ fract-ol/includes/colour_code.h
@@ -6,17 +6,17 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:10:58 by mishimod          #+#    #+#             */
-/*   Updated: 2025/04/03 16:17:37 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/04/03 16:27:11 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef "COLOUR_CODE_H"
-# define "COLOUR_CODE_H"
+#ifndef COLOUR_CODE_H
+# define COLOUR_CODE_H
 
 # define BLACK 0x000000
 # define BLUE 0x0000FF
 # define GREEN 0x00FF00
-# define PINK 0x000000
+# define PINK 0xFFA4FF
 # define RED 0xFF0000
 # define WHITE 0xFFFFFF
 # define YELLOW 0xFFFF00
--- fract-ol/srcs/render_fractol.c
+++ fract-ol/srcs/render_fractol.c
@@ -6,11 +6,12 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 23:03:40 by mishimod          #+#    #+#             */
-/*   Updated: 2025/04/02 23:06:10 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/04/03 16:23:54 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+#include "colour_code.h"
 
 void	render_fractol(void *mlx, t_data *img, void *mlx_win)
 {
@@ -26,7 +27,7 @@ void	render_fractol(void *mlx, t_data *img, void *mlx_win)
 		j = 0;
 		while (j <= HEIGHT)
 		{
-			my_mlx_pixel_put(img, i, j, 0x00800080);
+			my_mlx_pixel_put(img, i, j, PINK);
 			j++;
 		}
 		i++;
