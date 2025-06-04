052c0ea1434d5232c53b84ae95dd65f07adb5060
Mio Shimoda <mishimod@c2r5s3.42tokyo.jp>
2025-06-04 19:09:20 +0900

    [add] echo

--- minishell/builtin/echo.c
+++ minishell/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 22:20:14 by mishimod          #+#    #+#             */
-/*   Updated: 2025/06/03 22:43:48 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/06/04 19:08:51 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	get_echo(char **argv)
+bool	check_option(const char *option)
 {
 	int	i;
 
-	if (!argv[1])
-		return (1);
-
-	i = 0;
-	if (ft_strncmp(argv[1], -n, 2))
+	i = 1;
+	if (ft_strncmp(option, "-n", 2) == 0)
 	{
+		i++;
+		while (option[i])
+		{
+			if (option[i] != 'n')
+				return (false);
+			i++;
+		}
+		return (true);
 	}
+	return (false);
 }
 
-int	main(void)
+int	get_echo(const char **argv)
 {
-	return (get_echo);
+	int	i;
+	int	j;
+
+	if (argv[1])
+	{
+		i = 1;
+		if (check_option(argv[1]) == true)
+			i++;
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				write(1, &argv[i][j], 1);
+				j++;
+			}
+			if (argv[i + 1])
+				write(1, " ", 1);
+			i++;
+		}
+	}
+	if (!argv[1] || check_option(argv[1]) == false)
+		write(1, "\n", 1);
+	return (0);
 }
+
+// int	main(int argc, const char **argv)
+// {
+// 	return (get_echo(argv));
+// }
