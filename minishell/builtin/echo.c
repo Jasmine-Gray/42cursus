6e6107884d14231e58f4303dfcf0f0deeeba50c2
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2025-06-04 15:16:58 +0900

    [wip]

---
+++ minishell/builtin/echo.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 22:20:14 by mishimod          #+#    #+#             */
+/*   Updated: 2025/06/03 22:43:48 by mishimod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/ft_printf/ft_printf.h"
+#include "../libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	get_echo(char **argv)
+{
+	int	i;
+
+	if (!argv[1])
+		return (1);
+
+	i = 0;
+	if (ft_strncmp(argv[1], -n, 2))
+	{
+	}
+}
+
+int	main(void)
+{
+	return (get_echo);
+}
