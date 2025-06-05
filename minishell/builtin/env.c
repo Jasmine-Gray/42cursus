8d7c71b550444f971545d9e54956d6bb7c680c48
Mio Shimoda <mishimod@c2r6s3.42tokyo.jp>
2025-06-05 17:55:21 +0900

    [wip] env

--- minishell/builtin/env.c
+++ minishell/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 22:14:23 by mishimod          #+#    #+#             */
-/*   Updated: 2025/06/05 16:41:57 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/06/05 17:27:31 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,36 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-int main()
-{
-		const char *name;
-		name = "PATH";
-		printf("%s=%s\n", name, getenv(name));
-		return (0);
-}
+// int main()
+// {
+// 		const char *name;
+// 		name = "PATH";
+// 		printf("%s=%s\n", name, getenv(name));
+// 		return (0);
+// }
 
+int main(int argc, char *argv[], char *envp[])
+{
+    int i = 0;
+
+    /*  argc の確認 */
+    printf("argc = %d\n", argc);
+
+    /* argv の確認 */
+    printf("argv = [");
+    for (i = 0; i < argc; i++)
+    {
+        printf("\'%s\' ", argv[i]);
+    }
+    printf("]\n");
+
+    /* envp の確認 */
+    printf("envp = [");
+    for (i = 0; envp[i]; i++)
+    {
+        printf("\'%s\' \n", envp[i]);
+    }
+    printf("]\n");
+
+    return 0;
+}
