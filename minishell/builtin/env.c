4aa8e304f29f29b33362b51e0280e104e5573ce1
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2025-06-24 22:59:34 +0900

    [wip] env

--- minishell/builtin/env.c
+++ minishell/builtin/env.c
@@ -6,21 +6,32 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 22:14:23 by mishimod          #+#    #+#             */
-/*   Updated: 2025/06/24 20:23:52 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/06/24 22:59:22 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "minishell.h"
-#include "stdlib.h"
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int main()
-{
-		const char *name;
-		name = "PATH";
-		printf("%s=%s\n", name, getenv(name));
-		return (0);
+extern char **environ;
+
+int main(void) {
+    char **env = environ;
+    while (*env) {
+        printf("%s\n", *env); // 1つの環境変数（KEY=VALUE形式）
+        env++;
+    }
+    return 0;
 }
+
+// int main()
+// {
+// 		const char *name;
+// 		name = "PATH";
+// 		printf("%s=%s\n", name, getenv(name));
+// 		return (0);
+// }
 
 
 // int main() {
@@ -32,3 +43,6 @@ int main()
 //     }
 //     return 0;
 // }
+
+// getenvにname(環境変数名)を渡してあげる。
+// nameはenvironの挙動を実装する。
