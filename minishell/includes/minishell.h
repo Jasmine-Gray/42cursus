6fc98f605a5c28d78598634b9e5636f74006c110
Mio Shimoda <mishimod@c2r5s7.42tokyo.jp>
2025-07-15 20:17:15 +0900

    [add] output list env

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mishimod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 22:12:29 by mishimod          #+#    #+#             */
-/*   Updated: 2025/07/15 16:19:44 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/07/15 18:50:28 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,21 @@
 typedef struct s_env
 {
 	char			*value;
+	char			*name;
 	struct s_env	*next;
 }					t_env;
+
+typedef struct s_map
+{
+	t_env			item_head;
+}					t_map;
+
+t_env				*item_new(char *name, char *value);
+t_map				*map_new(void);
+char				*map_get(t_map *map, const char *name);
+int					map_put(t_map *map, const char *string);
+int					map_set(t_map *map, const char *name, const char *value);
+int					map_unset(t_map *map, const char *name);
 
 int					get_echo(const char **argv);
 int					get_pwd(void);
