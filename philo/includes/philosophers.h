ac455a3e5439a692cdf5065d68bf67ec9144bdd9
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2025-09-03 15:34:18 +0900

    [wip]

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,11 +6,14 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 14:47:42 by mishimod          #+#    #+#             */
-/*   Updated: 2025/08/28 14:48:25 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/09/03 15:19:49 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+
+#include <pthread.h>
+#include "stdio.h"
 
 #endif
--- philo/main.c
+++ philo/main.c
@@ -6,12 +6,59 @@
 /*   By: mishimod <mishimod@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 14:46:13 by mishimod          #+#    #+#             */
-/*   Updated: 2025/08/28 14:47:29 by mishimod         ###   ########.fr       */
+/*   Updated: 2025/09/03 15:19:47 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int main()
-{
+int a = 0;
+pthread_mutex_t mutex;
+
+void *func_thread(void *p) {
+  printf("start %d\n", *(int*)p);
+
+  int i=0;
+  for(i=0; i < 10000; i++){
+    pthread_mutex_lock(&mutex);
+	int next = a + 1;
+	int now = a;
+	a = next;
+    pthread_mutex_unlock(&mutex);
+
+	if (now+1 != next) {
+	  printf("other theard change %d %d\n", a+1, next);
+	}
+  }
+
+  return 0;
+}
+
+int main(void) {
+  pthread_mutex_init(&mutex, NULL);
+  
+  printf("test\n");
+
+  int b = 42;
+
+  pthread_t pthread;
+  pthread_create( &pthread, NULL, &func_thread, &b);
+
+  int i=0;
+  for(i=0; i < 10000; i++){
+    pthread_mutex_lock(&mutex);
+	int next = a + 1;
+	int now = a;
+	a = next;
+    pthread_mutex_unlock(&mutex);
+
+	if (now+1 != next) {
+	  printf("other theard change %d %d\n", a+1, next);
+	}
+  }
+
+  pthread_join(pthread, NULL); // pthreadで作られたスレッドが終わるまで待つ
+  printf("a=%d\n", a);
+
+  return 0;
 }
