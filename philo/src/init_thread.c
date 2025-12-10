cbc4137f9a4c1b2fe7246ae4577938288eb51ae1
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2025-12-10 15:52:10 +0900

    [wip]

--- philo/src/init_thread.c
+++ philo/src/init_thread.c
@@ -1,41 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_table.c                                       :+:      :+:    :+:   */
+/*   init_thread.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mishimod <mishimod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/27 21:15:45 by mishimod          #+#    #+#             */
-/*   Updated: 2025/12/06 20:12:37 by mishimod         ###   ########.fr       */
+/*   Created: 2025/12/06 01:01:10 by mishimod          #+#    #+#             */
+/*   Updated: 2025/12/06 20:45:21 by mishimod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static long        get_current_time(void)
+// void *thread_func(void* arg)
+// {
+// 		(void)arg;
+// 		printf("hello world\n");
+// 		// eating
+// 		// sleeping
+// 		// thinking
+// 		// waiting
+// 
+// 		return NULL;
+// }
+
+int create_thread(t_arg *arg)
 {
-        struct timeval        tv;
+		int i;
 
-        gettimeofday(&tv, NULL);
-        return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+		i = 0;
+		while(i < arg->num_of_philo)
+		{
+				if (pthread_create(&arg->philo[i], NULL, thread_func, &arg->philo[i]) != 0)
+						return (1);
+				i++;
+		}
+		return (0);
 }
-
-void	init_table(t_arg *arg)
-{
-	int	i;
-
-	i = 0;
-	while (i < arg->num_of_philo)
-	{
-		pthread_mutex_init(&arg->fork[i], NULL);
-		i++;
-	}
-
-	arg->start_time = get_current_time();
-
-	return ;
-}
-
-
-
-
